b4c77c6a1d81c0076c58cdb528d51dcf6fa63930
Igor <Drinkinswo@gmail.com>
2022-08-13 15:04:24 +0300

    1. Модульные тесты. [#159435 #329640] fix#3

--- src/test/java/ru/job4j/condition/ConverterTest.java
+++ src/test/java/ru/job4j/condition/ConverterTest.java
@@ -1,9 +1,7 @@
 package ru.job4j.condition;
 
-import org.junit.Assert;
 import org.junit.Test;
-
-import static ru.job4j.condition.Converter.*;
+import org.junit.Assert;
 
 public class ConverterTest {
 
@@ -11,16 +9,16 @@ public class ConverterTest {
     public void whenConvert140rblThen2dot3333Dollar() {
         float in = 140;
         float expected = 2.3333f;
-        float out = rubleToDollar(in);
+        float out = Converter.rubleToDollar(in);
         float eps = 0.0001f;
         Assert.assertEquals(expected, out, eps);
     }
-    
+
     @Test
     public void whenConvert140RblThen2Euro() {
         float in = 140;
         float expected = 2.0f;
-        float out = rubleToEuro(in);
+        float out = Converter.rubleToEuro(in);
         float eps = 0.0001f;
         Assert.assertEquals(expected, out, eps);
     }
